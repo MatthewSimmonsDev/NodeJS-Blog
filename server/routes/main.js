5eaef9eb837466c20f2be21dc85bdc63fcaa96e7
Matthew Simmons <75648307+MatthewSimmonsDev@users.noreply.github.com>
2024-06-05 16:39:58 -0400

    Beginning EJS
    Started adding ejs layout.

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 //Routes
 router.get("/", (req, res) => {
-  res.send("Hello World!");
+  const locals = {
+    title: "NodeJS-Blog",
+    description: "This is a simple blog using Node.js, express and MongoDB",
+  };
+
+  res.render("index", { locals });
 });
 
 module.exports = router;
